39c6a37ae1bac6ce6273a2f024b6aa1364fd232e
PRRaisin <philippe.raisin@icloud.com>
2020-04-20 15:13:09 +0100

    Initial commit PRRAISIN

--- preprocessing.py
+++ preprocessing.py
@@ -18,6 +18,53 @@ import pickle
 
 log = logging.getLogger(__name__)
 
+def get_pop(map_file,left_x,top_y,window,plot=False):
+    """
+    get_pop(raster filename,left_x,top_y,window,plot=False)
+
+    Given a raster file, and row,cols ranges,
+    return the lonlat of the ranges, nancount, and the nunsum
+
+    Optionally plot the raster window [False]
+    """
+    right_x,bottom_y = left_x + window, top_y + window
+
+    with rasterio.open(map_file) as src:
+        left_lon, top_lat = src.xy(top_y,left_x)
+        right_lon, bottom_lat = src.xy(bottom_y, right_x )
+        center_lon , center_lat = (right_lon + left_lon)/2., (top_lat+bottom_lat)/2.
+        #Window(col_off, row_off, width, height)
+        w = src.read(1, window=Window(left_x, top_y, window, window))
+        if plot:
+            pyplot.imshow(w, cmap='pink')
+            pyplot.show()
+        nancount=np.count_nonzero(~np.isnan(w))
+        count = np.size(w)
+        tot_pop=np.nansum(w)
+    if count == 0:
+        return {} #Out of bounds
+    if tot_pop == 0 or window < 1: #Mark the window no further split.
+        split=False
+    else:
+        split=True
+    out={'window':window,
+         'left_x':left_x,
+         'right_x':right_x,
+         'top_y':top_y,
+         'bottom_y':bottom_y,
+         'left_lon':left_lon,
+         'top_lat':top_lat,
+         'right_lon':right_lon,
+         'bottom_lat':bottom_lat,
+         'center_lon':center_lon ,
+         'center_lat':center_lat,
+         'count': count,
+         'nancount':nancount,
+         'tot_pop':tot_pop,
+         'split': split}
+    return out
+
+
 def split(map_file,origin,plot=False):
     """
     Split a window row in 4 parts, and return new rows results
